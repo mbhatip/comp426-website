484df903f65a27c54e34c49de7a096529ac576d6
Mehmet Hatip <mbhatip@gmail.com>
2021-05-31 16:24:03 -0400

    change api

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React, { useEffect, useState } from 'react'
 import Table from './Table'
 //import Form from './Form'
 
+const MC_DOMAIN = "memohat.xyz"
+
 function App() {
     const [status, setStatus] = useState(null)
     const [isLoading, setLoading] = useState(true)
     useEffect(() => {
         async function getStatus() {
             try {
-                const res = await fetch(process.env.REACT_APP_DOMAIN_MC_SERVER_STATUS);
+                const res = await fetch("https://mcapi.us/server/status/?ip=" + MC_DOMAIN);
                 setStatus(await res.json())
             } catch (error) {
                 setStatus(null)
@@ -18,7 +20,7 @@ function App() {
         getStatus()
     }, [isLoading])
 
-    const statusMsg = isLoading ? "Loading..." : !status ? "Unable to reach server" : status.running ? "Running" : status.idle ? "Idle" : "Down"
+    const statusMsg = isLoading ? "Loading..." : !status ? "Unable to reach API" : status.online ? "Running" : "Down"
 
     if (!status || !status.running) {
         return <div className="container">
@@ -30,8 +32,8 @@ function App() {
     }
     return <div className="container">
         <h1>Status: {statusMsg}</h1>
-        <h1>Memory: {status.memory}</h1>
-        <h1>Users: {status.users.length}/20</h1>
+        <h1>MOTD: {status.motd}</h1>
+        <h1>Users: {status.players.length}/20</h1>
         <Table users={status.users} />
         <button onClick={() => setLoading(true)}>
             refresh
