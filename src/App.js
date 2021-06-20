6c268932782f4b636b5b47d7647d73956efb5a79
Mehmet Hatip <mbhatip@gmail.com>
2021-06-20 14:18:33 -0400

    complete

--- src/App.js
+++ src/App.js
@@ -1,43 +1,127 @@
 import React, { useEffect, useState } from 'react'
-import Table from './Table'
-//import Form from './Form'
+import invert from 'invert-color'
 
-const MC_DOMAIN = "memohat.xyz"
+import Table from './Table'
+import demo from './demo';
+
+// Firebase App (the core Firebase SDK) is always required and must be listed first
+import firebase from "firebase/app";
+// If you are using v7 or any earlier version of the JS SDK, you should import firebase using namespace import
+// import * as firebase from "firebase/app"
+
+// Add the Firebase products that you want to use
+import "firebase/auth";
+import { FirebaseAuth } from "react-firebaseui";
+import * as firebaseui from 'firebaseui';
+
+firebase.initializeApp({
+  apiKey: 		process.env.REACT_APP_FIREBASE_APIKEY,
+  authDomain: 		process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
+  projectId: 		process.env.REACT_APP_FIREBASE_PROJECTID,
+  storageBucket: 	process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
+  messagingSenderId: 	process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID,
+  appId: 		process.env.REACT_APP_FIREBASE_APPID
+});
 
 function App() {
-    const [status, setStatus] = useState(null)
-    const [isLoading, setLoading] = useState(true)
+    const [user, setUser] = useState(null)
+    const [domain, setDomain] = useState(null)
+    const [s, setStatus] = useState(null)
+    const [isLoading, setLoading] = useState(false)
+    const [color, setColor] = useState("#FFFFFF")
+
     useEffect(() => {
-        async function getStatus() {
-            try {
-                const res = await fetch("https://mcapi.us/server/status/?ip=" + MC_DOMAIN);
-                setStatus(await res.json())
-            } catch (error) {
+        document.body.style.backgroundColor = color
+        document.body.style.color = invert(color)
+    }, [color])
+
+    async function getStatus() {
+        if (domain === "demo") {
+            setStatus(demo)
+            return
+        }
+        setLoading(true)
+        try {
+            const res = await fetch(`https://eu.mc-api.net/v3/server/ping/${domain}`);
+            const obj = await res.json()
+            if (obj.online) {
+                obj.domain = domain
+                setStatus(obj)
+            } else {
                 setStatus(null)
             }
-            setLoading(false)
+        } catch (error) {
+            setStatus(null)
         }
-        getStatus()
-    }, [isLoading])
-
-    const statusMsg = isLoading ? "Loading..." : !status ? "Unable to reach API" : status.online ? "Running" : "Down"
-
-    if (!status || !status.running) {
-        return <div className="container">
-            <h1>Status: {statusMsg}</h1>
-            <button onClick={() => setLoading(true)}>
-                refresh
-        </button>
-        </div>
+        setLoading(false)
     }
     return <div className="container">
-        <h1>Status: {statusMsg}</h1>
-        <h1>MOTD: {status.motd}</h1>
-        <h1>Users: {status.players.length}/20</h1>
-        <Table users={status.users} />
-        <button onClick={() => setLoading(true)}>
-            refresh
-        </button>
+        <h1>Minecraft Server Status</h1>
+		{!user && <div>
+			<p>Sign in to save color theme: </p>
+            <FirebaseAuth 
+                uiConfig={{
+                    signInFlow: 'popup',
+                    signInOptions: [
+                        firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+                        firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+                        firebase.auth.GithubAuthProvider.PROVIDER_ID,
+                        firebaseui.auth.AnonymousAuthProvider.PROVIDER_ID,
+                    ],
+                    callbacks: {
+                        signInSuccessWithAuthResult: (authResult, redirectURL) => {
+                            const user = authResult.user
+                            setUser(user)
+                            if (user.isAnonymous) return false
+                            if (!/^#[a-fA-F0-9]{6}$/.test(user.photoURL)) {
+                                user.updateProfile({
+                                    photoURL: "#FFFFFF"
+                                })
+                                setColor("#FFFFFF")
+                            } else {
+                                setColor(user.photoURL)
+                            }
+                            return false
+                        }
+                    },
+                    
+                }}
+                firebaseAuth={firebase.auth()} 
+            />
+		</div>}
+        {user && <div>
+            <input 
+            onChange={event => setColor(event.target.value)}
+            type="color"
+            defaultValue={/^#[a-fA-F0-9]{6}$/.test(user.photoURL) ? user.photoURL : "#000000"}
+            />  
+            <h1>Logged in as {user.isAnonymous ? "Guest" : user.displayName}... 
+                <button onClick={() => {
+                    if (!user.isAnonymous) {
+                        user.updateProfile({
+                            photoURL: color
+                        })
+                    }
+                    firebase.auth().signOut()
+                    setUser(null)
+                    setColor("#FFFFFF")
+                }}>Logout?</button>
+            </h1>
+            <input 
+                onKeyPress={(event) => {if(event.key === "Enter") getStatus()}}
+                onChange={(event) => setDomain(event.target.value)}
+                placeholder="Enter domain here"
+            />
+            <button onClick={getStatus}>Get Status</button>
+            <h1>Status: {isLoading ? "Loading..." : s ? "Online" : "Down"} </h1>
+            {s && <div> 
+                <h1>Domain: {s.domain}</h1>
+                <h1>Favicon: <img src={s.favicon} alt="server favicon"/></h1>
+                <h1>Version: {s.version.name}</h1> 
+                <h1>Users: {s.players.online}/{s.players.max}</h1>
+                <Table users={s.players.sample} />
+            </div>}
+        </div>}
     </div>
 }
 
--- src/Table.js
+++ src/Table.js
@@ -1,45 +1,66 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 
 function TableHeader() {
     return (
         <thead>
             <tr>
-                <th>Username</th>
-                <th>Avatar</th>
+                <th>Username (hover for history)</th>
+                <th>Avatar (hover for skin, click for download)</th>
             </tr>
         </thead>
     )
 }
 
-function TableBody(props) {
-    return (
-        <tbody>
-            {
-                props.users.map((user, index) => {
-                    return (
-                        <tr key={index}>
-                            <td>{user['username']}</td>
-                            <td><img src={"https://www.mc-heads.net/avatar/" + user['uuid']}
-                            alt={user['username'] + " avatar"}
-                            width="100" height="100"></img></td>
-                        </tr>
-                    )
-                })
+
+function TableElement(props) {
+    const uuid = props.user['id']
+    const [names, setNames] = useState(null)
+    const [hover, setHover] = useState(false)
+    useEffect(() => {
+        if (names) {return}
+        async function getNames() {
+            try {
+                const res = await fetch(`https://www.mc-heads.net/minecraft/profile/${uuid}`);
+                const json_obj = await res.json()
+                setNames(json_obj['name_history'].map((o) => o['name']))
+            } catch (error) {
+                setNames(null)
             }
-        </tbody>
+        }
+        getNames()
+    })
+    return (
+        <tr
+            onClick={() => {window.open(`https://www.mc-heads.net/download/${uuid}`)}}
+            onMouseEnter={() => setHover(true)}
+            onMouseLeave={() => setHover(false)}
+        >
+            <td key="name">{names && hover ? names.toString().split(",").map((n, i) => {return <span key={i}><br />{n}</span>}) : props.user['name']}</td>
+            <td key="img"><img
+                src={`https://www.mc-heads.net/${hover ? "body" : "avatar"}/${uuid}`}
+                alt={props.user['name'] + " avatar"}
+                width="100" height={hover ? "200" : "100"}>
+            </img></td>
+        </tr>
     )
 }
 
 function Table(props){
-    if (!props.users || !props.users.length) {
+    if (!props.users) {
+        return null
+    } 
+    const valid = props.users.filter((u) => u['id'] !== "00000000-0000-0000-0000-000000000000")
+    if (!valid.length) {
         return null
     }
     return (
         <table>
-            <TableHeader />
-            <TableBody
-                users={props.users}
-            />
+            <TableHeader key="header" />
+            <tbody>
+                {valid.map((user, index) => {
+                    return <TableElement key={index} user={user} />
+                })}
+            </tbody>
         </table>
     )
 }
